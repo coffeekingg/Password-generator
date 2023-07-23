66b81d4c2b40c08c2c7587973304d317dda7fcc8
coffeekingg <138327341+coffeekingg@users.noreply.github.com>
2023-07-23 10:19:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import random
 
-print("Вы хотите создать свой пароль или сгенерировать?")
-print("Нажмите Y для ввода своего пароля")
-print("Нажмите G для генерации пароля:")
-answer = input()
-big = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-small = 'abcdefghijklmnopqrstuvwxyz'
-nums = '1234567890'
-symbols = '!@#№$%^&?*()~'
-str = big + small + nums + symbols
 
 def pass_check(password):
+    big = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    small = 'abcdefghijklmnopqrstuvwxyz'
+    nums = '1234567890'
+    symbols = '!@#№$%^&?*()~'
     flag = False
     bigkol = 0
     smallkol = 0
@@ -32,26 +27,71 @@ def pass_check(password):
     else:
         return True
 
-if answer == 'Y':
-    print("Введите Ваш пароль. Минимальная длина 8 символов")
-    password = input()
+def btm_check():
+    password = str(loginInput.get())
     if pass_check(password) == True:
-        print("Пароль достаточно надёжен")
+        logoutOutput['text'] ="Пароль достаточно надёжен"
     else:
         password = "-----"
-        print('Пароль недостаточно надёжен')
-    print("Ваш пароль:" + password)
+        logoutOutput['text'] ='Пароль недостаточно надёжен'
+    logoutRez['text'] = "Ваш пароль:" + password
 
-elif answer == 'G':
-    print("Введите длину желаемого пароля:")
-    length =int(input())
+def btm_gen():
+    big = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    small = 'abcdefghijklmnopqrstuvwxyz'
+    nums = '1234567890'
+    symbols = '!@#№$%^&?*()~'
+    str = big + small + nums + symbols
+    length = 8
     password =''
-    if length < 8:
-        print("Пароль недостаточно надёжен. Минимальная длина - 8 символов")
-    else:
-        while pass_check(password) != True:
-            password = ''
-            password = ''.join(random.choice(str) for i in range(length))
-        print("Ваш пароль:" + password)
-else:
-    print("Введите Y для проверки Вашего пароля или G для генерации")
+    #if length < 8:
+    #    logoutOutput['text'] ="Пароль недостаточно надёжен. Минимальная длина - 8 символов"
+    #else:
+    while pass_check(password) != True:
+        password = ''
+        password = ''.join(random.choice(str) for i in range(length))
+    logoutOutput['text'] = 'Генерация завершена'
+    logoutRez['text'] = f"Ваш пароль:{password}"
+
+
+from tkinter import *
+
+root = Tk()
+
+
+root.title("Генератор паролей")
+root.geometry("700x450+700+290")
+root.resizable(True, True)
+
+
+
+frame = Frame(root, bg = '#ffb300', bd = 5)
+frame.place(relx = 0.1, rely = 0.1, relwidth=0.8, relheight=0.5)
+
+title = Label(frame, text = "Вы хотите проверить свой пароль или сгенерировать?\n"
+                            "Для проверки введите пароль и нажмите кнопку\n"
+                            "Для генерации введите длину и нажмите кнопку\n"
+                            "Минимальная длина пароля 8 символов"
+
+                           , bg = "white", font = 40, relief = RAISED)
+title.place(x = 70, y = 10)
+
+frame2 = Frame(root, bg = '#ffb700', bd = 5)
+frame2.place(relx = 0.1, rely = 0.7, relwidth= 0.8, relheight=0.25)
+
+loginInput = Entry(frame, bg ='white')
+loginInput.place(x = 170, y = 175,  width=170)
+
+logoutOutput = Label(frame2, text = 'Результаты проверки / генерации пароля', font = 40, width=40, height=3, relief = RAISED)
+logoutOutput.pack()
+
+logoutRez = Label(frame2, text = 'Пароль', font = 40, width=40, height=3, relief = RAISED)
+logoutRez.pack()
+
+btn1 = Button(frame, text = "Проверить пароль", font = 40, command = btm_check() )
+btn1.place(x = 70, y = 115, width= 200)
+
+#btn2 = Button(frame, text = "Сгенерировать пароль", font = 40, command = btm_gen() )
+#btn2.place(x = 290, y = 115)
+
+root.mainloop()
